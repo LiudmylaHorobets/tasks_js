1498f4e28a5ddd2450824929c0edfc27f838e895
Liudmyla Horobets <horobets1323@gmail.com>
2024-06-11 11:22:47 +0300

    tasks 45-47

--- 45-includes-object-or-array/finish.js
+++ 45-includes-object-or-array/finish.js
@@ -9,26 +9,35 @@
  * а затем применить метод "includes" с аргументом, который также будет преобразован в строку
  */
 
+function isElementInArray(searchElement, inputArray) {
+  if (typeof searchElement !== "object") {
+    return inputArray.includes(searchElement);
+  }
+
+  const convertedArray = inputArray.map((element) => JSON.stringify(element));
+  return convertedArray.includes(JSON.stringify(searchElement));
+}
+
 const tags = [
-  ['javascript', 'es6'],
-  ['css', 'flexbox'],
-  ['html', 'web-browser'],
-]
+  ["javascript", "es6"],
+  ["css", "flexbox"],
+  ["html", "web-browser"],
+];
 
 const fruits = [
-  { title: 'Orange', quantity: 10 },
-  { title: 'Banana', quantity: 5 },
-  { title: 'Apple', quantity: 25 },
-]
+  { title: "Orange", quantity: 10 },
+  { title: "Banana", quantity: 5 },
+  { title: "Apple", quantity: 25 },
+];
 
-const primitiveTypesArray = [25, 'x', true, undefined, null]
+const primitiveTypesArray = [25, "x", true, undefined, null];
 
-console.log(isElementInArray(['css', 'flexbox'], tags)) // true
+console.log(isElementInArray(["css", "flexbox"], tags)); // true
 
-console.log(isElementInArray(['flexbox', 'css'], tags)) // false
+console.log(isElementInArray(["flexbox", "css"], tags)); // false
 
-console.log(isElementInArray({ title: 'Apple', quantity: 25 }, fruits)) // true
+console.log(isElementInArray({ title: "Apple", quantity: 25 }, fruits)); // true
 
-console.log(isElementInArray({ title: 'Banana' }, fruits)) // false
+console.log(isElementInArray({ title: "Banana" }, fruits)); // false
 
-console.log(isElementInArray(25, primitiveTypesArray)) // true
+console.log(isElementInArray(25, primitiveTypesArray)); // true
--- 46-push-to-array-if-not-exists/finish.js
+++ 46-push-to-array-if-not-exists/finish.js
@@ -10,16 +10,24 @@
  * ПРИМЕЧАНИЕ: Мы предполагаем, что "inputArray" содержит элементы только примитивных типов
  */
 
-const myNumbers = [123, 50, 27]
+const pushIfUnique = (inputArray, newElement) => {
+  if (inputArray.includes(newElement)) {
+    return console.log(`${newElement} already on the array`);
+  }
 
-pushIfUnique(myNumbers, 50) // "50 уже в массиве"
-console.log(myNumbers) // [123, 50, 27]
+  return inputArray.push(newElement);
+};
 
-pushIfUnique(myNumbers, 80)
-console.log(myNumbers) // [123, 50, 27, 80]
+const myNumbers = [123, 50, 27];
 
-pushIfUnique(myNumbers, 80) // "80 уже в массиве"
-console.log(myNumbers) // [123, 50, 27, 80]
+pushIfUnique(myNumbers, 50); // "50 уже в массиве"
+console.log(myNumbers); // [123, 50, 27]
 
-pushIfUnique(myNumbers, 77)
-console.log(myNumbers) // [123, 50, 27, 80, 77]
+pushIfUnique(myNumbers, 80);
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 80); // "80 уже в массиве"
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 77);
+console.log(myNumbers); // [123, 50, 27, 80, 77]
