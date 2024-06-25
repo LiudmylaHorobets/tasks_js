d5e63062bf49184e7af32c40f8cd402bf4de588c
Liudmyla Horobets <horobets1323@gmail.com>
2024-06-25 12:24:17 +0300

    tasks 59, 61

--- 61-sum-numbers/finish.js
+++ 61-sum-numbers/finish.js
@@ -8,11 +8,24 @@
  * 3. Также верните эту сумму как результат функции
  */
 
-sumNumbers(1, 3)
+// const  = () => {
+//   console.log(arguments);
+// };
+
+function sumNumbers() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  console.log(sum);
+  return sum;
+}
+
+sumNumbers(1, 3);
 // 4
 
-sumNumbers(10, 20, 5)
+sumNumbers(10, 20, 5);
 // 35
 
-sumNumbers(2, 5, 80, 1, 10, 12)
+sumNumbers(2, 5, 80, 1, 10, 12);
 // 110
